545dd153147826bfdf259f46c61c51fae122c5ab
Sabrina <sabrinacunsolo85@gmail.com>
2021-01-07 16:09:27 +0100

    inizio layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="footer-top">
+
+    </div>
+    <div class="footer-bottom">
+
+    </div>
+</header>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="header-top">
+        <div class="">
+            <ul>
+                <li>
+                    <a href="#">Dc Power℠ Visa®</a>
+                </li>
+                <li>
+                    <a href="#">Additional Dc Sites</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="header-bottom">
+
+    </div>
+</header>
